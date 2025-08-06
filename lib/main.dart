cbbc5c071f85fd9928cec22a0ea5e67dcc663fd7
DouglasEmmanuel <emmanueldouglas2121@gmail.com>
2025-08-06 14:01:41 -0700

    created a todo-app using flutter

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+// lib/main.dart
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import '../screens/todo_screen.dart';
+
+void main() {
+  runApp(
+    ProviderScope(  // Essential: Wrap your app in ProviderScope
+      child: MyApp(),
+    ),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Riverpod Todo Showcase',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        useMaterial3: true,
+      ),
+      home: TodoScreen(),
+    );
+  }
+}
